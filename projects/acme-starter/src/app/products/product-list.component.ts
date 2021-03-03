8df0aab4c776bc6efc17d22cd613f9962cebae03
Dong Zeng <dong@freshbytesoftware.com>
2021-03-03 11:25:13 -0600

    add product-list component

---
+++ projects/acme-starter/src/app/products/product-list.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+})
+export class ProductListComponent {}
